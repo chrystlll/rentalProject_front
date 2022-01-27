28bb8d0904eca6460bf23a653290208cf7fc9912
unknown <chrystelle.bihan@gmail.com>
2022-01-27 12:13:36 +0100

    modification archi

--- rental/rental_app/src/app/app.module.ts
+++ rental/rental_app/src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ManageTenantComponent } from './manage-tenant/manage-tenant.component';
 import { ManageReceiptComponent } from './manage-receipt/manage-receipt.component';
 import { LoginComponent } from './login/login.component';
 import { ManagePropertiesComponent } from './manage-properties/manage-properties.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './_components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { TenantFormComponent } from './manage-tenant/tenant-form/tenant-form.component';
-import {DpDatePickerModule} from 'ng2-date-picker';
+import { DpDatePickerModule } from 'ng2-date-picker';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { TenantDetailsComponent } from './manage-tenant/tenant-details/tenant-details.component'; 
 
 @NgModule({
